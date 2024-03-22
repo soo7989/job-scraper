2b063d48f1117318eacdf19391525ec4bab8f58b
monkey <hs.sanggong@gmail.com>
2024-03-22 18:52:22 +0900

    scraping web3, details
    광고 정보 제외
    link 주소 추출 방법 수정
    job description 추가

--- scraping_web3.py
+++ scraping_web3.py
@@ -15,8 +15,8 @@ def job_info(jobs):
         jobid = job.get("data-jobid")
         company = job.findAll("td")[1].text.strip()
         position = job.findAll("td")[0].text.strip()
-        description = job.findAll("td")[3].text.strip()
         link = transform_string(position, jobid)
+        description = scraper_detail(link)
         job_data = {
             "company": company,
             "position": position,
@@ -27,18 +27,36 @@ def job_info(jobs):
         sch_jobs.append(job_data)
 
 
+def scraper_detail(url):
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
+    description = soup.find(attrs={"style": "word-wrap: break-word;"}).text.strip()
+    max_desc = f"{limit_text_length(description)}..."
+    return max_desc
+
+
+def limit_text_length(text, max_length=400):
+    return text[:max_length]
+
+
 # 구인정보
 def scraper(url):
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
-    jobs = soup.find(class_="row-cols-2").find("table").find("tbody").findAll("tr")[0:5]
+    jobs = (
+        soup.find(class_="row-cols-2")
+        .find("table")
+        .find("tbody")
+        .findAll("tr", attrs={"data-jobid": True})[0:5]
+    )
     job_info(jobs)
 
 
 def transform_string(position, jobid):
     lowercased_string = position.lower()
     hyphenated_string = lowercased_string.replace(" ", "-")
-    final_string = f"{BASE_URL}/{hyphenated_string}/{jobid}"
+    dotted_string = hyphenated_string.replace(".", "-")
+    final_string = f"{BASE_URL}/{dotted_string}/{jobid}"
     return final_string
 
 
